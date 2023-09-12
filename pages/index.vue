b7d394677f91c5e47644a146340c22dbfb93ceeb
iiGibran <90310030+iiGibran@users.noreply.github.com>
2023-09-12 11:57:44 +0100

    navbar fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Navbar /> <!-- Include the Navbar component here -->
     <!-- Rest of your index page content -->
     <div class="container mx-auto p-4">
       <h1 class="text-3xl font-semibold">Welcome to Our Gym Website</h1>
